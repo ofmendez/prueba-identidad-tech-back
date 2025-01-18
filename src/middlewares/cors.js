1b7b067833830871da27cb17133a30e68b198dbe
Fabian Mendez <ofmendez@gmail.com>
2025-01-18 00:04:47 -0500

    Update CORS middleware to allow requests from specific domains

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -4,7 +4,7 @@ export const corsMiddleware = () =>
   cors({
     origin: (o) => {
       let result = 'http://localhost:5173';
-      if (o.endsWith('.pages.dev') || o.startsWith('https://localhost'))
+      if (o.startsWith('https://identidad-tech-front.pages.dev') || o.startsWith('https://localhost'))
         result = o;
       return result;
     }
